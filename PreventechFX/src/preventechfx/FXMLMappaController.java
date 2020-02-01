9a86029c7eb8303a0d119cfecb417487977eb2c2
VRalf10 <49068098+VRalf10@users.noreply.github.com>
2020-02-01 13:37:26 +0100

    Aggiornata Mappa pulsante indietro + logo + titolo

--- PreventechFX/src/preventechfx/FXMLMappaController.java
+++ PreventechFX/src/preventechfx/FXMLMappaController.java
@@ -5,30 +5,52 @@
  */
 package preventechfx;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.web.WebEngine;
 import javafx.scene.web.WebView;
+import javafx.stage.Stage;
 
 /**
  * FXML Controller class
  *
- * @author raffa
+ * @author mitic
  */
 public class FXMLMappaController implements Initializable {
-    
-    @FXML 
-    private WebView webView;
+
+    @FXML
+    private WebView map;
+    @FXML
+    private Button buttonBack;
+    @FXML
+    Stage stage;
+    Parent root;
+
     /**
      * Initializes the controller class.
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
-        WebEngine engine = webView.getEngine();
+        WebEngine engine = map.getEngine();
         engine.load("http://localhost:8080/Preventech/MapServlet");
     }    
+
+    @FXML
+    private void actionBack(MouseEvent event) throws IOException {
+        stage = (Stage) buttonBack.getScene().getWindow();
+        root = FXMLLoader.load(getClass().getResource("FXMLDocument.fxml"));
+        
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show(); 
+    }
     
 }
--- PreventechFX/src/preventechfx/PreventechFX.java
+++ PreventechFX/src/preventechfx/PreventechFX.java
@@ -5,10 +5,12 @@
  */
 package preventechfx;
 
+
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 /**
@@ -25,6 +27,8 @@ public class PreventechFX extends Application {
         Scene scene = new Scene(root);
         
         stage.setScene(scene);
+        stage.getIcons().add(new Image("img/favicon.png"));
+        stage.setTitle("Preventech");
         stage.show();
     }
 
