0d4dc2ccd05639fdbd8883d20f92f29e6fb9b26c
Leyxargon <43447240+Leyxargon@users.noreply.github.com>
2020-02-06 12:53:54 +0100

    Pulizia singleton

--- PreventechFX/src/preventechfx/Database.java
+++ PreventechFX/src/preventechfx/Database.java
@@ -9,9 +9,6 @@ import com.mongodb.MongoClient;
 import com.mongodb.MongoClientURI;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
-import java.util.ArrayList;
-import java.util.List;
-import org.bson.Document;
 
 /**
  *
